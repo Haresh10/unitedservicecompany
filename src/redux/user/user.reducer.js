03ccb78b8ed34f23799c9be646d292276a4023ce
Apple <apple@Apples-MacBook-Pro.local>
2021-02-21 11:56:40 +0530

    Installed redux, react-redux, redux-saga, reselect, redux-logger,firebase and styled-components  libraries. Basic configurations related to redux, redux-sagas completed.

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,11 @@
+const INITIAL_STATE = {
+  currentUser: null,
+  errorMessage: undefined,
+};
+const UserReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default UserReducer;
